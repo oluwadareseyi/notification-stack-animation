8380084b352fbea3435379a06cac7046d4e91616
Oluwadare Seyi <oluwadareseyii@gmail.com>
2024-02-18 09:57:05 +0100

    Initial Commit

---
+++ Notification Stack Animation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Notification Stack Animation
+//
+//  Created by Oluwaseyi Oluwadare on 18/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Notification Stack Animation/Notification_Stack_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Notification_Stack_AnimationApp.swift
+//  Notification Stack Animation
+//
+//  Created by Oluwaseyi Oluwadare on 18/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Notification_Stack_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
